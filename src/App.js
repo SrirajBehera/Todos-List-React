e2e6b2ef0dc7f6252f8464b13cc7d0c8f9ccad8e
Sriraj <behera.sriraj2911@gmail.com>
2021-06-19 02:43:52 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header';
+import { Todos } from './MyComponents/Todos';
+import { Footer } from './MyComponents/Footer';
+import { AddTodo } from './MyComponents/AddTodo';
+import { About } from './MyComponents/About';
+import React, { useState, useEffect } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let initTodo
+    if (localStorage.getItem("todos") === null) {
+        initTodo = []
+    }
+    else {
+        initTodo = JSON.parse(localStorage.getItem("todos"))
+    }
+    const onDelete = (todo) => {
+        console.log("I am onDelete of todo", todo)
+        // traditional method which will not work
+        // Deleting this way in react doesn't work
+        // let index = todos.indexOf(todo)
+        // todos.splice(index, 1)
+
+        setTodos(todos.filter((e) => {
+            return e !== todo
+        }))
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }
+
+    const addTodo = (title, desc) => {
+        console.log("I am adding this Todo", title, desc)
+        let sno
+        if (todos.length === 0) {
+            sno = 1
+        }
+        else {
+            sno = todos[todos.length - 1].sno + 1
+        }
+        const myTodo = {
+            sno: sno,
+            title: title,
+            desc: desc
+        }
+        setTodos([...todos, myTodo])
+        console.log(myTodo)
+    }
+    // using react state hooks
+    // setTodos is a function
+    const [todos, setTodos] = useState(initTodo)
+    // using useEffect hooks to make sure updating is smoothly done
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos])
+    return (
+        <>
+            <Router>
+                <Header title="MyTodosList" searchBar={true} />
+                <Switch>
+                    {/* exact will help route properly as it reads from the starting / */}
+                    <Route exact path="/" render={() => {
+                        return (
+                            <>
+                                <AddTodo addTodo={addTodo} />
+                                <Todos todos={todos} onDelete={onDelete} />
+                            </>
+                        )
+                    }}>
+                    </Route>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                </Switch>
+
+                <Footer />
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component.
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam aliquam fuga quaerat consectetur magni asperiores cumque accusamus accusantium eos placeat, deserunt quidem ab. Quas excepturi nisi asperiores cupiditate ratione commodi aperiam minima soluta dolorem.</p>
+        </div>
+    )
+}
